ce0aa8a99e830b7c709612ecfae1c4095c9d7ecd
Jamie Bertram <jamie.bertram@tweag.io>
2022-01-12 10:40:17 -0500

    Fix default.nix typo

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ let
   url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
   sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2";
   src = ./.;
-  flake = import (pkgs.fetchTarball { inherit url sha256; }) { inherit src; };
+  flake = import (pkgs.fetchzip { inherit url sha256; }) { inherit src; };
 in
 {
   inherit flake;
